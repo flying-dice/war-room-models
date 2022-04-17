fd5a78db13092b6744667c0439dabfc0d0fc567b
🐸 <dice-a6db@outlook.com>
2022-04-18 00:00:18 +0100

    feat: add operation file export

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from "./entity";
 export * from "./ground-unit";
 export * from "./installation";
 export * from "./locatable";
+export * from "./operation-file";
 export * from "./static-unit";
 export * from "./turn-information";
 export * from "./unit";
