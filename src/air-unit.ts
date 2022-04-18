3966be9ec9a61d56073abb34e9fdcda321b4c6e9
🐸 <dice-a6db@outlook.com>
2022-04-18 20:18:26 +0100

    feat: add id to airunit

--- src/air-unit.ts
+++ src/air-unit.ts
@@ -1,3 +1,8 @@
 import { Unit } from "./unit";
 
+/**
+ * @id AirUnit
+ * @title AirUnit
+ * @description Air Unit Model such as Fighter, Bomber or CAS Wing
+ */
 export interface AirUnit extends Unit {}
